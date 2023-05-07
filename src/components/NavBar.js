5e58d45c2df22a8876742be8331cd6f4fa8029dd
Ben Saacks <108644584+Blancher@users.noreply.github.com>
2023-05-07 09:21:53 -0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 export default function NavBar() {
     return (
         <nav className='flex'>
-            <NavLink to="/" className={({isActive}) => isActive ? 'navlink dark' : 'navlink'}>Home</NavLink>
+            <NavLink to="/dcs_housing" className={({isActive}) => isActive ? 'navlink dark' : 'navlink'}>Home</NavLink>
             <NavLink to="/education" className={({isActive}) => isActive ? 'navlink dark' : 'navlink'}>Education</NavLink>
             <NavLink to="/transportation" className={({isActive}) => isActive ? 'navlink dark' : 'navlink'}>Transportation</NavLink>
             <NavLink to="/pricing" className={({isActive}) => isActive ? 'navlink dark' : 'navlink'}>Pricing</NavLink>
